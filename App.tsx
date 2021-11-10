4856a39bf9b2a286bcacbb2a50feb5b03349a9db
João Rodrigues <joao.duarte353@gmail.com>
2021-11-10 22:23:53 +0000

    android build

--- App.tsx
+++ App.tsx
@@ -16,35 +16,16 @@ import {
 } from '@react-navigation/native';
 import {PersistGate} from 'redux-persist/integration/react';
 import {fetchR3eData} from './src/store/slices/General';
-import {Platform, useColorScheme} from 'react-native';
+import {useColorScheme} from 'react-native';
 import LoadingAppComponent from './src/components/shared/LoadingAppComponent';
 import {withIAPContext} from 'react-native-iap';
-import SpInAppUpdates, {
-  IAUUpdateKind,
-  StartUpdateOptions,
-} from 'sp-react-native-in-app-updates';
+import UpdateContainer from './src/components/UpdateContainer';
 
 const App: React.FC = () => {
   const navRef = createNavigationContainerRef<ServerStackList>();
   const colorScheme = useColorScheme();
-  const inAppUpdates = new SpInAppUpdates(false);
 
   useFlipper(navRef);
-
-  useEffect(() => {
-    inAppUpdates.checkNeedsUpdate().then(result => {
-      if (result.shouldUpdate) {
-        let updateOptions: StartUpdateOptions = {};
-        if (Platform.OS === 'android') {
-          // android only, on iOS the user will be prompted to go to your app store page
-          updateOptions = {
-            updateType: IAUUpdateKind.FLEXIBLE,
-          };
-        }
-        inAppUpdates.startUpdate(updateOptions);
-      }
-    });
-  }, []);
 
   const getData = async () => {
     await store.dispatch(fetchR3eData());
@@ -60,11 +41,13 @@ const App: React.FC = () => {
     <StoreProvider store={store}>
       <PersistGate loading={<LoadingAppComponent />} persistor={persists}>
         <Provider theme={colorScheme === 'light' ? DefaultTheme : DarkTheme}>
-          <NavigationContainer
-            ref={navRef}
-            theme={colorScheme === 'light' ? NavDefault : NavDark}>
-            <DrawerStack />
-          </NavigationContainer>
+          <UpdateContainer>
+            <NavigationContainer
+              ref={navRef}
+              theme={colorScheme === 'light' ? NavDefault : NavDark}>
+              <DrawerStack />
+            </NavigationContainer>
+          </UpdateContainer>
         </Provider>
       </PersistGate>
     </StoreProvider>
---
+++ src/components/UpdateContainer.tsx
@@ -0,0 +1,29 @@
+import React, {useEffect} from 'react';
+import SpInAppUpdates, {
+  IAUUpdateKind,
+  StartUpdateOptions,
+} from 'sp-react-native-in-app-updates';
+import {Platform} from 'react-native';
+
+const UpdateContainer: React.FC = ({children}) => {
+  const inAppUpdates = new SpInAppUpdates(false);
+
+  useEffect(() => {
+    inAppUpdates.checkNeedsUpdate().then(result => {
+      if (result.shouldUpdate) {
+        let updateOptions: StartUpdateOptions = {};
+        if (Platform.OS === 'android') {
+          // android only, on iOS the user will be prompted to go to your app store page
+          updateOptions = {
+            updateType: IAUUpdateKind.FLEXIBLE,
+          };
+        }
+        inAppUpdates.startUpdate(updateOptions);
+      }
+    });
+  }, []);
+
+  return <>{children}</>;
+};
+
+export default UpdateContainer;
